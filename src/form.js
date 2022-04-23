c829d5bc4d071bd1923e823d991677f187eb88d6
karthik <karthik35314@gmail.com>
2022-04-24 01:16:28 +0530

    second -commit

--- src/form.js
+++ src/form.js
@@ -1,173 +1,219 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import csc from "country-state-city";
 import validate from "./validation";
 
 const Form = ({}) => {
-    const[values,setValues]=useState("")
-    const [state, setState] = React.useState({
+  const [values, setValues] = useState("");
+  const [states, setState] = React.useState({});
 
-    });
- 
-    const handle = (e) => {
-        
-      e.preventDefault();
-      const { name, value } = e.target;
-      setState( { ...state, [name]: value });
-      const data = validate(name, value);
-      setState((previousData) => ({
-        ...previousData,
-        [name + "Valid"]: data.valid,
-        [name + "Error"]: data.error,
-      }));
-    };
-    console.log(state);
-
-
-
-
-//use debouce
-
-
-  
-    const countries = csc.getAllCountries(); 
-    const updatedCountries = countries.map((country) => ({
-      label: country.name,
-      value: country.id,
-      ...country
-    }));
-    const updatedStates = (countryId) =>
-      csc
-        .getStatesOfCountry(countryId)
-        .map((state) => ({ label: state.name, value: state.id, ...state }));
-    const updatedCities = (stateId) =>
-      csc
-        .getCitiesOfState(stateId)
-        .map((city) => ({ label: city.name, value: city.id, ...city }));
-  
-  
-  
-    useEffect(() => {}, [values]);
-
-    const colourStyles = {
-        control: styles => ({ ...styles, backgroundColor: 'white' ,height: '45px',width: "100%",outline: "none !important",border: "2px solid #9b59b6"}),
-        option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-       
-          return {
-            ...styles,
-        
-            color: 'voilet',
-            cursor: isDisabled ? 'not-allowed' : 'default',
-           
-          }
-        },
-      
-        
-      };
-    
-const handlesubmit=(e)=>{
+  const handle = (e) => {
     e.preventDefault();
+    const { name, value } = e.target;
+    setState({ ...states, [name]: value });
+    const data = validate(name, value);
+    setState((previousData) => ({
+      ...previousData,
+      [name + "Valid"]: data.valid,
+      [name + "Error"]: data.error,
+    }));
+  };
 
+  
 
-}
+  const countries = csc.getAllCountries();
+  const updatedCountries = countries.map((country) => ({
+    label: country.name,
+    value: country.id,
+    ...country,
+  }));
+  const updatedStates = (countryId) =>
+    csc
+      .getStatesOfCountry(countryId)
+      .map((state) => ({ label: state.name, value: state.id, ...state }));
+  const updatedCities = (stateId) =>
+    csc
+      .getCitiesOfState(stateId)
+      .map((city) => ({ label: city.name, value: city.id, ...city }));
 
-let {fullnameValid , contactEmailValid ,phoneNumberValid,messageValid,contactEmail,fullname,phoneNumber,message} = state
-     
-let empty=contactEmail && fullname && phoneNumberValid &&message
- let valid = fullnameValid && contactEmailValid && phoneNumber && messageValid
+  useEffect(() => {}, [values]);
+  console.log(values);
+  const colourStyles = {
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: "white",
+      height: "45px",
+      width: "100%",
+      outline: "none !important",
+      border: "2px solid #9b59b6",
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles,
+
+        color: "voilet",
+        cursor: isDisabled ? "not-allowed" : "default",
+      };
+    },
+  };
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+  };
+  const handling = (value, event) => {
+    setValues({ state: value, city: null }, false);
+    console.log(value, event);
+  };
+  let { country, state, city } = values;
+  console.log(country, state, city);
+  let {
+    fullnameValid,
+    contactEmailValid,
+    phoneNumberValid,
+    messageValid,
+    contactEmail,
+    fullname,
+    phoneNumber,
+    message,
+  } = states;
+
+  let empty =
+    contactEmail &&
+    fullname &&
+    phoneNumberValid &&
+    messageValid &&
+    country !== undefined &&
+    state !== null &&
+    city !== null;
+  let valid = fullnameValid && contactEmailValid && phoneNumber && messageValid;
  
   return (
- 
     <div class="container">
-    <div class="title">Registration</div>
-    <div class="content">
-      <form action="#">
-        <div class="user-details">
-          <div class="input-box">
-            <span class="details">Full Name</span>
-            <input type="text" name="fullname" onChange={(e) => handle(e)}  placeholder="Enter your name" />
-            <span class="text-danger" >    {state.fullnameError ? state.fullnameError : ""}</span>
+      <div class="title">Registration</div>
+      <div class="content">
+        <form action="#">
+          <div class="user-details">
+            <div class="input-box">
+              <span class="details">Full Name</span>
+              <input
+                type="text"
+                name="fullname"
+                onChange={(e) => handle(e)}
+                placeholder="Enter your name"
+              />
+              <span class="details text-danger">
+                {" "}
+                {states.fullnameError ? states.fullnameError : ""}
+              </span>
+            </div>
+            <div class="input-box">
+              <span class="details">Email</span>
+              <input
+                type="text"
+                name="contactEmail"
+                onChange={(e) => handle(e)}
+                placeholder="Enter your email"
+              />
+              <span class="details text-danger">
+                {" "}
+                {states.contactEmailError ? states.contactEmailError : ""}
+              </span>
+            </div>
+            <div class="input-box">
+              <span class="details">Phone number</span>
+              <input
+                type="text"
+                name="phoneNumber"
+                onChange={(e) => handle(e)}
+                placeholder="Enter your phone number "
+              />
+              <span class=" details text-danger">
+                {" "}
+                {states.phoneNumberError ? states.phoneNumberError : ""}
+              </span>
+            </div>
+            <div class="input-box ">
+              <span class="details">Country</span>
+              <Select
+                id="country"
+                name="country"
+                label="country"
+                options={updatedCountries}
+                value={values.country}
+                onChange={(value) => {
+                  setValues(
+                    { ...values, country: value, state: null, city: null },
+                    false
+                  );
+                }}
+                styles={colourStyles}
+              />
+              {states.country}
+            </div>
+            <div class="input-box">
+              <span class="details">state</span>
+              <Select
+                id="state"
+                name="state"
+                options={updatedStates(
+                  values.country ? values.country.value : null
+                )}
+                value={values.state}
+                onChange={(value, event) => {
+                  // handling(value,event),
+                  setValues({ ...values, state: value, city: null }, false);
+                }}
+                styles={colourStyles}
+              />
+            </div>
+            <div class="input-box">
+              <span class="details">city</span>
+              <Select
+                id="city"
+                name="city"
+                options={updatedCities(
+                  values.state ? values.state.value : null
+                )}
+                value={values.city}
+                onChange={(value) => {
+                  setValues({ ...values, city: value }, false);
+                }}
+                styles={colourStyles}
+              />
+            </div>
           </div>
-          <div class="input-box">
-            <span class="details">Email</span>
-            <input type="text"   name="contactEmail" onChange={(e) => handle(e)} placeholder="Enter your email" />
-            <span class="text-danger" >  {state.contactEmailError ? state.contactEmailError : ""}</span>
+          <div class=" input-box ">
+            <span class="details">Message</span>
+            <textarea
+              class="input-box-area"
+              name="message"
+              placeholder="Enter Message"
+              onChange={(e) => handle(e)}
+            ></textarea>
+            <span class="details text-danger">
+              {" "}
+              {states.messageError ? states.messageError : ""}
+            </span>
           </div>
-          <div class="input-box">
-            <span class="details">Phone number</span>
-            <input type="text" name="phoneNumber" onChange={(e) => handle(e)} placeholder="Enter your phone number " />
-            <span class="text-danger" >      {state.phoneNumberError ? state.phoneNumberError : ""}</span>
-          </div>
-          <div class="input-box ">
-            <span class="details">Country</span>
-            <Select
-          id="country"
-          name="country"
-          label="country"
-          
-          options={updatedCountries}
-          value={values.country}
-          onChange={(value) => {
-           
-            setValues({ country: value, state: null, city: null }, false);
-          }}
-          styles={colourStyles}
-        />
-        {state.country}
-          </div>
-          <div class="input-box">
-            <span class="details">state</span>
-            <Select
-          id="state"
-          name="state"
-          options={updatedStates(values.country ? values.country.value : null)}
-          value={values.state}
-          onChange={(value) => {
-            
-              setValues({ state: value, city: null }, false);
-        
-          }}
-          styles={colourStyles}
-        />
-          </div>
-          <div class="input-box">
-            <span class="details">city</span>
-            <Select
-          id="city"
-          name="city"
-          options={updatedCities(values.state ? values.state.value : null)}
-          value={values.city}
-          onChange={(value) => {
-            setValues({  city: value}, false);
-          }}
-          styles={colourStyles}
-         
-        />
-          </div>
-
-       
-
-
-        </div>
-        <div class=" input-box " >
-        <span class="details">Message</span> 
-       <textarea class="input-box-area" name="message" placeholder="Enter Message" onChange={(e)=>handle(e)} ></textarea>
-       <span class="text-danger" >  {state.messageError? state.messageError : ""}</span> 
-        </div>
-        {
-            valid && empty ?
-            <div class="button">
-          
-          <button  onClick={(e)=>{handlesubmit(e)}} value="Register" >click</button>
-        </div>
-        :"fill"
-        }
-            <button class="button"  onClick={(e)=>{handlesubmit(e)}} value="Register" >click</button>
-      </form>
+          {valid && empty ? (
+            <button
+              class="btn btn-success"
+              type="submit"
+              onClick={(e) => {
+                handlesubmit(e);
+              }}
+            >
+              Register{" "}
+            </button>
+          ) : (
+            <button class="btn btn-secondary" type="submit">
+              Register{" "}
+            </button>
+          )}
+        </form>
+      </div>
     </div>
-  </div>
+  );
+};
 
-  )
-}
-
-export default Form
+export default Form;
--- src/validation.js
+++ src/validation.js
@@ -2,39 +2,37 @@ import React from "react";
 const validate = (fieldName, value, value2) => {
   let valid;
   let error;
-  let errorTwo;
+
   let validData;
   const re = /^[0-9\b]+$/;
+  const reg = /^[a-zA-Z0-9_]{5,}[a-zA-Z]+[0-9]*$/;
   switch (fieldName) {
-
-   case "message":
-    valid = value.length >2;
-    error = valid ? '' : 'Please enter valid ' + fieldName;
-    validData = {
-      valid,
-      error,
-    };
-    return validData;
-
-    case "state":
-      valid = value ==={};
-      error = valid ? '' : 'Please enter valid ' + fieldName;
+    case "message":
+      valid = value.length > 28;
+      error = valid ? "" : "Please enter 28 characters " + fieldName;
       validData = {
         valid,
         error,
       };
       return validData;
 
+    case "state":
+      valid = value === {};
+      error = valid ? "" : "Please enter valid " + fieldName;
+      validData = {
+        valid,
+        error,
+      };
+      return validData;
 
     case "fullname":
-        valid = value.length >= 3 && value.length <= 10;
-        error = valid ? '' : 'Please enter valid ' + fieldName;
-        validData = {
-          valid,
-          error,
-        };
-        return validData;
-
+      valid = reg.test(value) && value.length <= 8;
+      error = valid ? "" : "Please enter valid " + fieldName;
+      validData = {
+        valid,
+        error,
+      };
+      return validData;
 
     case "address":
       valid = value.length >= 3;
@@ -45,8 +43,6 @@ const validate = (fieldName, value, value2) => {
       };
       return validData;
 
-  
-
     case "contactEmail":
       valid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/);
       error = valid ? "" : "Please enter valid email address.";
@@ -56,20 +52,6 @@ const validate = (fieldName, value, value2) => {
       };
       return validData;
 
-  
-    case "zipcode":
-    case "countryCode":
-    case "city":
-    case "state":
-      valid = value.length >= 3 && value.length <= 10;
-      error = valid ? "" : "Please enter valid " + fieldName;
-      validData = {
-        valid,
-        error,
-      };
-      return validData;
-
-  
     case "phoneNumber":
       valid = value.length === 10 && value.match(/^[0-9]*\.?[0-9]*$/);
       error = valid ? "" : " Please enter 10 digit valid number";
